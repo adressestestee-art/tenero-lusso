d299664615d274c57b08dace0d82ab057346778a
adressestestee-art <adressestestee@gmail.com>
2025-09-03 01:49:32 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,22 +8,22 @@ app.use(express.json());
 
 const ORDERS_FILE = 'backend/orders.json';
 
-// Endpoint pour enregistrer une commande
 app.post('/orders', (req, res) => {
     const order = req.body;
     fs.readFile(ORDERS_FILE, 'utf8', (err, data) => {
         let orders = [];
-        if (!err && data) {
+        if(!err && data) {
             orders = JSON.parse(data);
         }
         orders.push(order);
         fs.writeFile(ORDERS_FILE, JSON.stringify(orders, null, 2), err => {
-            if (err) return res.status(500).send("Erreur serveur");
+            if(err) return res.status(500).send("Erreur serveur");
             res.status(200).send("Commande enregistrée");
         });
     });
 });
 
-app.listen(3000, () => {
-    console.log("Serveur lancé sur http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Serveur lancé sur le port ${PORT}`);
 });

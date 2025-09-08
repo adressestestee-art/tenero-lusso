d23db666b5da3b71d75643fcb2758cb6222ac0bc
adressestestee-art <adressestestee@gmail.com>
2025-09-08 21:15:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+import { setupDropdownMenu } from './menu.js';
+import { startCarousel } from './carousel.js';
+import { loadProductDetails } from './product.js';
 
+document.addEventListener('DOMContentLoaded', () => {
+    // Lancer la logique du menu sur toutes les pages
+    setupDropdownMenu();
+
+    // Lancer le carrousel uniquement sur la page d'accueil
+    if (document.body.classList.contains('homepage')) {
+        startCarousel();
+    }
+
+    // Lancer la logique des produits uniquement sur la page de détails
+    if (document.body.classList.contains('product-detail-page')) {
+        loadProductDetails();
+    }
+});

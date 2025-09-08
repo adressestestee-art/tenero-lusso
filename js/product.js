529dd654c28ebaceea6aeea146671ec5583b3edb
adressestestee-art <adressestestee@gmail.com>
2025-09-08 22:44:48 +0200

    Update product.js

--- js/product.js
+++ js/product.js
@@ -4,14 +4,14 @@ const products = [
         name: 'T-shirt Basique',
         price: '49,99 €',
         description: 'Un classique revisité pour une qualité irréprochable. Ce T-shirt est coupé dans un jersey de coton épais, offrant un tombé parfait et une durabilité exceptionnelle.',
-        mainImage: '../images/t-shirt.png' // Ajout de ../
+        mainImage: '../images/t-shirt.png'
     },
     {
         id: 'jogging-confort',
         name: 'Jogging Confort',
         price: '69,99 €',
         description: 'Notre jogging parfait pour se détendre avec style. Conçu avec un molleton doux et durable, il offre une liberté de mouvement et un confort optimal.',
-        mainImage: '../images/jogger.png' // Ajout de ../
+        mainImage: '../images/jogger.png'
     }
 ];
 

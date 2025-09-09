07e7ca8b764e6c17cd2bd2dbd97bf190ec7249f4
adressestestee-art <adressestestee@gmail.com>
2025-09-09 12:07:09 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-// js/main.js
-
-import './carousel.js';
-import './menu.js';
 document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.getElementById('menu-toggle');
     const navLinks = document.querySelector('.nav-links');
